2e2e892c9c359afc736c02ebb407dfb36f21b970
apstemmer <apstemmer@hotmail.com>
2023-09-24 21:55:21 -0400

    And.. Day 2b completed

--- rust/2022/src/bin/02.rs
+++ rust/2022/src/bin/02.rs
@@ -13,6 +13,7 @@ fn main() {
         .collect();
 
     let mut score: i32 = 0;
+    let mut score_b: i32 = 0;
 
     for game in &strategy {
 
@@ -28,7 +29,21 @@ fn main() {
             ("C", "Z") => score += 3 + 3,
             _ => println!("Should never happen!!")
         }
+
+        match game {
+            ("A", "X") => score_b += 3 + 0,
+            ("B", "X") => score_b += 1 + 0,
+            ("C", "X") => score_b += 2 + 0,
+            ("A", "Y") => score_b += 1 + 3,
+            ("B", "Y") => score_b += 2 + 3,
+            ("C", "Y") => score_b += 3 + 3,
+            ("A", "Z") => score_b += 2 + 6,
+            ("B", "Z") => score_b += 3 + 6,
+            ("C", "Z") => score_b += 1 + 6,
+            _ => println!("Should never happen!!")
+        }
     }
 
-    println!("Strategy's Score: {}", score);
+    println!("Strategy A's Score: {}", score);
+    println!("Strategy B's Score: {}", score_b);
 }
