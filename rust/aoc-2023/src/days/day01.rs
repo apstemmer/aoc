f539b9fa9bbd179761c4e8ba11b1d76492ba4401
Arthur Stemmer <stemmera@amazon.com>
2023-12-01 13:55:53 -0800

    Completed 1A

--- rust/aoc-2023/src/days/day01.rs
+++ rust/aoc-2023/src/days/day01.rs
@@ -1,5 +1,15 @@
 pub fn execute(input: Vec<String>) -> (Option<String>, Option<String>) {
     // TODO: Implement AOC
 
-    (None, None)
+    let mut sum_a: i32 = 0;
+    for calibration in &input {
+        let stripped: String = calibration.chars().filter(|c| c.is_numeric()).collect();
+        let cal_val = format!("{}{}", stripped.chars().next().unwrap(), stripped.chars().last().unwrap());
+
+        if let Ok(num) = cal_val.parse::<i32>(){
+            sum_a += num;
+            println!("{} -> {} -> {} -> {}", calibration, stripped, cal_val, sum_a);
+        };
+    }
+    (Some(sum_a.to_string()), None)
 }
