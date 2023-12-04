7a3bb6a5ce19509c531a82b922b5ff55fdd41a9d
apstemmer <apstemmer@hotmail.com>
2023-12-04 13:37:20 -0800

    Day 4 - Part A

--- rust/aoc-2023/src/days/day04.rs
+++ rust/aoc-2023/src/days/day04.rs
@@ -1,5 +1,24 @@
+use std::collections::HashSet;
 
 pub fn execute(input: Vec<String>) -> (Option<String>, Option<String>) {
 
-    (None, None)
+    let mut sum_a = 0;
+
+    for card in input {
+        let mut split_card: Vec<&str> = card.split(|c| c == ':' || c == '|').collect();
+        let mut winners: HashSet<i32> = split_card[1].split_whitespace()
+            .filter_map(|num_str| num_str.parse().ok())
+            .collect();
+        let mut numbers: HashSet<i32> = split_card[2].split_whitespace()
+            .filter_map(|num_str| num_str.parse().ok())
+            .collect();
+        let mut intersection: Vec<i32> = winners.intersection(&numbers).cloned().collect();
+
+        match intersection.len() {
+            p if p > 0 => sum_a += 2_i32.pow((intersection.len() - 1) as u32),
+            _ => ()
+        }
+    }
+
+    (Some(sum_a.to_string()), None)
 }
