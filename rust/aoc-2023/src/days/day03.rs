976ccfffa4b58324b90d425a7c4744f5285f3f90
apstemmer <apstemmer@hotmail.com>
2023-12-04 12:59:20 -0800

    Day 3 - Part B

--- rust/aoc-2023/src/days/day03.rs
+++ rust/aoc-2023/src/days/day03.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 pub fn execute(input: Vec<String>) -> (Option<String>, Option<String>) {
 
@@ -6,8 +7,11 @@ pub fn execute(input: Vec<String>) -> (Option<String>, Option<String>) {
         .collect();
 
     let mut sum_a = 0;
+    let mut sum_b = 0;
     let mut is_part = false;
     let mut curr_num = 0;
+    let mut gear_map: HashMap<(usize, usize), u32> = HashMap::new();
+    let mut gear:Option<(usize, usize)> = None;
 
     let get_indices = |r:i32, c:i32| -> Vec<(i32, i32)> {
         let mut indices: Vec<(i32, i32)> = Vec::new();
@@ -37,7 +41,15 @@ pub fn execute(input: Vec<String>) -> (Option<String>, Option<String>) {
             if item.is_numeric() {
                 curr_num = curr_num * 10 + item.to_digit(10).unwrap();
                 for (dr, dc) in get_indices(r as i32, c as i32) {
-                    let mut aux: char = schematic[((r as i32) + dr) as usize][((c as i32) + dc) as usize];
+                    let coord = (((r as i32) + dr) as usize, ((c as i32) + dc) as usize);
+                    let mut aux: char = schematic[coord.0][coord.1];
+                    if aux == '*' && gear != Some((coord.0, coord.1)){
+                        if gear != None {
+                            println!("{}, {}", r, c);
+                            panic!("Should Never Happen!!")
+                        }
+                        gear = Some((coord.0, coord.1));
+                    }
                     if !aux.is_numeric() && aux != '.' {
                         // We've found a symbol
                         is_part = true;
@@ -48,10 +60,20 @@ pub fn execute(input: Vec<String>) -> (Option<String>, Option<String>) {
                 if is_part {
                     sum_a += curr_num;
                 }
+                match gear {
+                    Some(gear_coord) if !gear_map.contains_key(&gear_coord) => {
+                        gear_map.insert(gear_coord, curr_num);
+                    },
+                    Some(gear_coord) => {
+                        sum_b += gear_map.get(&gear_coord).unwrap() * curr_num;
+                    },
+                    None => ()
+                }
                 curr_num = 0;
                 is_part = false;
+                gear = None;
             }
         }
     }
-    (Some(sum_a.to_string()), None)
+    (Some(sum_a.to_string()), Some(sum_b.to_string()))
 }
