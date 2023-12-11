ebbe802d57cabb57332aaf911be9ece071ec2c7d
apstemmer <apstemmer@hotmail.com>
2023-12-10 16:20:56 -0800

    Day 9 - Part B

--- rust/aoc-2023/src/days/day09.rs
+++ rust/aoc-2023/src/days/day09.rs
@@ -1,5 +1,6 @@
 pub fn execute(input: Vec<String>) -> (Option<String>, Option<String>) {
     let mut sum_a: i64 = 0;
+    let mut sum_b: i64 = 0;
     for row in input {
 
         let mut readings: Vec<i64> = row.split_whitespace()
@@ -19,10 +20,13 @@ pub fn execute(input: Vec<String>) -> (Option<String>, Option<String>) {
         for idx in (1..diffs.len()).rev() {
             let curr_diff = diffs[idx].clone();
             let next_base = diffs[idx - 1].last().unwrap().clone();
+            let prev_base = diffs[idx - 1].first().unwrap().clone();
             diffs[idx - 1].push(curr_diff[curr_diff.len() - 1] + next_base);
+            diffs[idx - 1].insert(0, prev_base - curr_diff[0])
         }
         println!("{:?} -> {:?}", readings, diffs[0]);
         sum_a += diffs[0].last().unwrap();
+        sum_b += diffs[0].first().unwrap();
     }
-    (Some(sum_a.to_string()), None)
+    (Some(sum_a.to_string()),Some(sum_b.to_string()))
 }
