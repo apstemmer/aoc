31bc8c629b907749d68aeb1b5d10241333df75fb
apstemmer <apstemmer@hotmail.com>
2023-12-15 14:34:20 +0100

    Day 15 - Part 1

--- rust/aoc-2023/src/days/day15.rs
+++ rust/aoc-2023/src/days/day15.rs
@@ -1,4 +1,19 @@
 pub fn execute(input: Vec<String>) -> (Option<String>, Option<String>) {
-
-    (None, None)
+    let sequences: Vec<Vec<i64>> = input
+        .iter()
+        .flat_map(|s| s.split(',')
+            .map(|substring| substring.chars().map(|c| c as i64).collect())
+        )
+        .collect();
+    let mut sum_a:i64 = 0;
+    for sequence in sequences {
+        let mut hash:i64 = 0;
+        for val in sequence.iter() {
+            hash += val;
+            hash = hash * 17;
+            hash = hash % 256;
+        }
+        sum_a += hash
+    }
+    (Some(sum_a.to_string()), None)
 }
